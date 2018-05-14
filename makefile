12d72788809afe775725b0b13a20399e1fd4b6de
tjc15b <tjc15b@my.fsu.edu>
2018-05-14 11:07:59 -0400

    Obfuscated code complete

---
+++ makefile
@@ -0,0 +1,2 @@
+prog: prog.c
+	${CC} prog.c -o prog
---
+++ prog.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+typedef int ll11l1;
+typedef char** lll1l1;
+#define lll11l (1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1)
+ll11l1 main(ll11l1 l1ll1l,lll1l1 l1l1ll){return(l1l1l1(atoi(lll11l[l1l1ll]),atoi(l1l1ll[lll11l+lll11l])));}ll11l1 l1l1l1(ll11l1 ll1l1l,ll11l1 l1llll){ll11l1 llll11;while(ll1l1l!=(lll11l-lll11l)){llll11=ll1l1l;ll1l1l=l1llll%ll1l1l;l1llll=llll11;}return l1llll;}
